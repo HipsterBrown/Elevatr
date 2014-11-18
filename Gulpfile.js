572f516dc473aa3a05e233398ef6b5fb05d246a6
Nick (Hipsterbrown) <nick@kratedesign.com>
2014-11-18 10:36:18 -0500

    swap gulp-server-livereload plugin with gulp-connect

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,38 +1,60 @@
 var gulp = require('gulp');
 var git = require('gulp-git');
-var autoprefix = require('gulp-autoprefixer');
-var jshint = require('gulp-jshint');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
+var autoprefix = require('gulp-autoprefixer');
+var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
-var server = require('gulp-server-livereload');
+var connect = require('gulp-connect');
 
+gulp.task('serve', function(){
+  'use strict';
+
+  connect.server({
+    root: ['demo'],
+    livereload: true
+  });
+});
+
+gulp.task('html', function(){
+  'use strict';
+
+  gulp.src('demo/index.html')
+      .pipe(connect.reload());
+});
 
 gulp.task('sass', function() {
   'use strict';
 
-  return gulp.src('./demo/*.scss')
-  .pipe(sass())
-  .pipe(autoprefix())
-  .pipe(gulp.dest('./demo'));
+  gulp.src('demo/*.scss')
+      .pipe(sass())
+      .pipe(autoprefix())
+      .pipe(gulp.dest('demo'))
+      .pipe(connect.reload());
+});
+
+gulp.task('hint', function(){
+  'use strict';
+
+  gulp.src(['src/elevatr.js', 'demo/demo.js'])
+      .pipe(hint('.jshintrc'))
+      .pipe(hint.reporter('default'));
 });
 
 gulp.task('js', function() {
   'use strict';
 
-  return gulp.src('./src/*.js')
-  .pipe(jshint('.jshintrc'))
-  .pipe(jshint.reporter('default'))
-  .pipe(uglify())
-  .pipe(gulp.dest('./demo'));
+  gulp.src(['src/elevatr.js'])
+      .pipe(uglify())
+      .pipe(gulp.dest('demo'));
 });
 
-gulp.task('serve', ['sass', 'js'], function() {
+gulp.task('watch', function() {
   'use strict';
 
-  gulp.src('demo')
-  .pipe(server({
-    livereload: true,
-    port: 4200
-  }));
+  gulp.watch(['demo/index.html'], ['html']);
+  gulp.watch(['demo/*.scss'], ['sass']);
+  gulp.watch(['dmeo/*.js', 'src/*.js'], ['hint', 'scripts']);
 });
+
+gulp.task('default', ['serve', 'watch']);
